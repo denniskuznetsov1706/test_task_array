da9863034b36a50c9d88b5d144ce0c6f94979f51
USER-PC\Professional <kuznetsov.dennis.1706@gmail.com>
2021-05-26 19:57:13 +0300

    little bit more

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ console.log("hello");
 function attempt(available, allowed, preffered)
 {
 
+if (allowed.includes('any'))
+{
+    allowed.splice(allowed.indexOf('any',1));
+    for (let i=available.indexOf(allowed[0])+1; i<available.length; i++)
+        allowed.push(available[i]);
+}    
 
 
 let intersection = available.filter(x => allowed.includes(x));
 
 if (intersection.length==0) return [];
 
+
 switch (preffered.length)
 {
-    case 1: 
+    case 1: //------------------------prefferd with 1 element
        var closet=preffered[0];
+
+       if (preffered.includes('any')) return intersection;
 
        for (let x of intersection)
           if (x<closet) {
@@ -22,15 +31,21 @@ switch (preffered.length)
           return [closet];
     break;
 
-    case 2: 
+    case 2: //--------------------preffered with 2 element, also  has any
     var closet=preffered;
     
-    for (let x of intersection)
-       if (x<closet) {
-           closet[0]=x;
-          break;
-       }
-    
+    if (preffered[0]!='any')
+        for (let x of intersection)
+        if (x<closet) {
+            closet[0]=x;
+            break;
+        }
+    else {
+
+     //    if (intersection.includes(preffered[1]))
+      //      for (let i=0; i<intersection.indexOf(preffered[1]))
+
+    }
     if (intersection.length==1) return [closet[0]];   
     if (preffered[0]> intersection[intersection.length-1]) return [intersection[intersection.length-1]];
 
@@ -64,7 +79,14 @@ console.log(attempt([240,720],[360,720],[1080]))//720
 console.log(attempt([240],[360,720],[1080]))//[]
 
 console.log(attempt([240,360,720],[240,360,720,1080],[240,360]))//[240, 360]
-console.log(attempt([240,720],[240,360,720,1080],[240,360]))//[240, 360]
+console.log(attempt([240,720],[240,360,720,1080],[240,360]))//[240, 720]
 console.log(attempt([240,720],[240,360,1080],[240,360]))//[240]
 console.log(attempt([720],[240,360,1080],[240,360]))//[]
 console.log(attempt([240,360],[240,360],[720,1080]))//[360]
+
+console.log("-----------------")
+console.log(attempt([240,360,720],[360,'any'],[360,720]))//[360,720]
+console.log(attempt([240,360,720],[360,720],['any']))//[360,720]
+
+
+console.log(attempt([240,360,720],[1080],['any', 720]))//[]
