8a4a7a0b3302c1a99cd7e2272b8111cf549974f1
USER-PC\Professional <kuznetsov.dennis.1706@gmail.com>
2021-06-08 16:33:38 +0300

    eslint styling code

--- attempt.js
+++ attempt.js
@@ -1,35 +1,39 @@
-module.exports.attempt = function (available, allowed, preffered) {
+/* eslint-disable linebreak-style */
+module.exports.attempt = function ( available, allowed, preffered )
+{
   //----------------------if allowed has 'any'-------------
-  if (allowed.includes('any')) {
-    allowed.splice(allowed.indexOf('any'), 1)
-    for (let i = available.indexOf(allowed[0]) + 1; i < available.length; i++)
-      allowed.push(available[i])
+  if( allowed.includes ( 'any' ) )
+  {
+    allowed.splice( allowed.indexOf( 'any' ), 1 )
+    for( let i = available.indexOf( allowed[ 0 ] ) + 1; i < available.length; i++ )
+      allowed.push( available[ i ] )
   }
   //------------------------------------------------------
   let result = []
-  let interSection = available.filter(x => allowed.includes(x)) // union operarion -> /
+  let interSection = available.filter( ( x ) => allowed.includes( x ) ) // union operarion -> /
   //----------------------if preffered has 'any'-------------
-  if (preffered.includes('any')) return interSection
+  if( preffered.includes( 'any' ) )return interSection
   //------------------------------------------
-  if (interSection.length == 0) return []
-  let closestLeft = interSection.filter(num => num <= preffered[0])
-  result.push(Math.min(...closestLeft))
+  if( interSection.length === 0 )return []
+  let closestLeft = interSection.filter( ( num ) => num <= preffered[ 0 ] )
+  result.push( Math.min( ... closestLeft ) )
   //---------------if preffered has more than 1 element------------------------
-  if (preffered.length > 1) {
+  if( preffered.length > 1 )
+  {
     let closestRight = interSection.filter(
-      num => num <= preffered[preffered.length - 1]
+      ( num ) => num <= preffered[ preffered.length - 1 ]
     )
-    if (
-      (interSection[interSection.length - 1] >
-        preffered[preffered.length - 1]) &
-      !interSection.includes(preffered[preffered.length - 1])
+    if(
+      ( interSection[ interSection.length - 1 ]
+        > preffered[ preffered.length - 1 ] )
+        & !interSection.includes( preffered[ preffered.length - 1 ] )
     )
-      result.push(interSection[interSection.length - 1])
-    else result.push(Math.max(...closestRight))
+      result.push( interSection[ interSection.length - 1 ] )
+    else result.push( Math.max( ... closestRight ) )
   }
   //--------------------------------------------------------------
-  result = Array.from(new Set(result)) //state only uniqe elements of the result array
-  if (Math.max(...allowed) <= preffered[0]) result = [Math.max(...result)]
+  result = Array.from( new Set( result ) ) //state only uniqe elements of the result array
+  if( Math.max( ... allowed ) <= preffered[ 0 ] ) result = [ Math.max( ... result ) ]
 
   return result
 }
--- my.test.js
+++ my.test.js
@@ -1,97 +1,101 @@
-let _ = require('wTesting')
+let _ = require( 'wTesting' )
 
-let attempt = require('./attempt')
+let attempt = require( './attempt' )
 
-function Test1 (test) {
+function Test1 ( test )
+{
   /* one element in preffered */
-  test.case = 'Test #1'
-  var got = attempt.attempt([240, 360, 720], [360, 720], [1080])
-  var expected = [360]
-  test.identical(got, expected)
+  test.case = 'Test #1';
+  var got = attempt.attempt( [ 240, 360, 720 ], [ 360, 720 ], [ 1080 ] )
+  var expected = [ 360 ];
+  test.identical( got, expected );
 
   test.case = 'Test #2'
-  var got = attempt.attempt([240, 720], [360, 720], [1080])
-  var expected = [720]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 720 ], [ 360, 720 ], [ 1080 ] )
+  var expected = [ 720 ]
+  test.identical( got, expected )
 
   test.case = 'Test #3'
-  var got = attempt.attempt([240], [360, 720], [1080])
+  var got = attempt.attempt( [ 240 ], [ 360, 720 ], [ 1080 ] )
   var expected = []
-  test.identical(got, expected)
+  test.identical( got, expected )
 }
 
-function Test2 (test) {
+function Test2 ( test )
+{
   /* more than one element in preffered */
   test.case = 'Test #4'
-  var got = attempt.attempt([240, 360, 720], [240, 360, 720, 1080], [240, 360])
-  var expected = [240, 360]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 360, 720 ], [ 240, 360, 720, 1080 ], [ 240, 360 ] )
+  var expected = [ 240, 360 ]
+  test.identical( got, expected )
 
   test.case = 'Test #5'
-  var got = attempt.attempt([240, 720], [240, 360, 720, 1080], [240, 360])
-  var expected = [240, 720]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 720 ], [ 240, 360, 720, 1080 ], [ 240, 360 ] )
+  var expected = [ 240, 720 ]
+  test.identical( got, expected )
 
   test.case = 'Test #6'
-  var got = attempt.attempt([240, 720], [240, 360, 1080], [240, 360])
-  var expected = [240]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 720 ], [ 240, 360, 1080 ], [ 240, 360 ] )
+  var expected = [ 240 ]
+  test.identical( got, expected )
 
   test.case = 'Test #7'
-  var got = attempt.attempt([720], [240, 360, 1080], [240, 360])
+  var got = attempt.attempt( [ 720 ], [ 240, 360, 1080 ], [ 240, 360 ] )
   var expected = []
-  test.identical(got, expected)
+  test.identical( got, expected )
 
   test.case = 'Test #8'
-  var got = attempt.attempt([240, 360], [240, 360], [720, 1080])
-  var expected = [360]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 360 ], [ 240, 360 ], [ 720, 1080 ] )
+  var expected = [ 360 ]
+  test.identical( got, expected )
 }
 
-function Test3 (test) {
+function Test3 ( test )
+{
   /* element has 'any'*/
   test.case = 'Test #9'
-  var got = attempt.attempt([240, 360, 720], [360, 'any'], [360, 720])
-  var expected = [360, 720]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 360, 720 ], [ 360, 'any' ], [ 360, 720 ] )
+  var expected = [ 360, 720 ]
+  test.identical( got, expected )
 
   test.case = 'Test #10'
-  var got = attempt.attempt([240, 360, 720], [240, 360, 720], ['any', 720])
-  var expected = [240, 360, 720]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 360, 720 ], [ 240, 360, 720 ], [ 'any', 720 ] )
+  var expected = [ 240, 360, 720 ]
+  test.identical( got, expected )
 
   test.case = 'Test #11'
-  var got = attempt.attempt([240, 360, 720], [360, 1080], ['any', 720])
-  var expected = [360]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 360, 720 ], [ 360, 1080 ], [ 'any', 720 ] )
+  var expected = [ 360 ]
+  test.identical( got, expected )
 
   test.case = 'Test #12'
-  var got = attempt.attempt([240, 360, 720], [1080], ['any', 720])
+  var got = attempt.attempt( [ 240, 360, 720 ], [ 1080 ], [ 'any', 720 ] )
   var expected = []
-  test.identical(got, expected)
+  test.identical( got, expected )
 
   test.case = 'Test #13'
-  var got = attempt.attempt([240, 360, 720], [360, 720], ['any'])
-  var expected = [360, 720]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 360, 720 ], [ 360, 720 ], [ 'any' ] )
+  var expected = [ 360, 720 ]
+  test.identical( got, expected )
 }
-function Test4 (test) {
+function Test4 ( test )
+{
   /* little bit of my tests */
   test.case = 'Test #14'
-  var got = attempt.attempt([240, 360], [240, 360], [360, 720,1080])
-  var expected = [360]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 360 ], [ 240, 360 ], [ 360, 720, 1080 ] )
+  var expected = [ 360 ]
+  test.identical( got, expected )
 
   test.case = 'Test #15'
-  var got = attempt.attempt([240, 360], [240, 360], [240, 360, 720,1080])
-  var expected = [240, 360]
-  test.identical(got, expected)
+  var got = attempt.attempt( [ 240, 360 ], [ 240, 360 ], [ 240, 360, 720, 1080 ] )
+  var expected = [ 240, 360 ]
+  test.identical( got, expected )
 
 }
 
 var testSuite = {
-  name: 'test_tast_array',
-  tests: {
+  name : 'test_tast_array',
+  tests : {
     Test1,
     Test2,
     Test3,
@@ -100,7 +104,7 @@ var testSuite = {
 }
 
 /* Initilize test suit */
-testSuite = wTestSuite(testSuite)
-if (typeof module !== 'undefined' && !module.parent)
+testSuite = wTestSuite( testSuite )
+if( typeof module !== 'undefined' && !module.parent )
   /* Run all tests of the suit */
-  wTester.test(testSuite.name)
+  wTester.test( testSuite.name )
