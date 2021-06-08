7b6be1ac77e51321eed95e7f0a8bb8c361d2c3ee
USER-PC\Professional <kuznetsov.dennis.1706@gmail.com>
2021-06-08 17:42:58 +0300

    eslint style code 3, maybe last)

--- attempt.js
+++ attempt.js
@@ -1,39 +1,51 @@
 /* eslint-disable linebreak-style */
+
 module.exports.attempt = function ( available, allowed, preffered )
 {
+
   //----------------------if allowed has 'any'-------------
+
   if( allowed.includes ( 'any' ) )
   {
-    allowed.splice( allowed.indexOf( 'any' ), 1 )
+    allowed.splice( allowed.indexOf( 'any' ), 1 );
     for( let i = available.indexOf( allowed[ 0 ] ) + 1; i < available.length; i++ )
-      allowed.push( available[ i ] )
+      allowed.push( available[ i ] );
   }
-  //------------------------------------------------------
-  let result = []
-  let interSection = available.filter( ( x ) => allowed.includes( x ) ) // union operarion -> /
+
+  //------------------// union operarion -> //------------------------------------
+
+  let result = [];
+  let interSection = available.filter( ( x ) => allowed.includes( x ) );
+
   //----------------------if preffered has 'any'-------------
-  if( preffered.includes( 'any' ) )return interSection
+
+  if( preffered.includes( 'any' ) )
+  return interSection;
+
   //------------------------------------------
-  if( interSection.length === 0 )return []
-  let closestLeft = interSection.filter( ( num ) => num <= preffered[ 0 ] )
-  result.push( Math.min( ... closestLeft ) )
+
+  if( interSection.length === 0 )
+  return [];
+
+  let closestLeft = interSection.filter( ( num ) => num <= preffered[ 0 ] );
+  result.push( Math.min( ... closestLeft ) );
+
   //---------------if preffered has more than 1 element------------------------
+
   if( preffered.length > 1 )
   {
-    let closestRight = interSection.filter(
-      ( num ) => num <= preffered[ preffered.length - 1 ]
-    )
-    if(
-      ( interSection[ interSection.length - 1 ]
-        > preffered[ preffered.length - 1 ] )
-        & !interSection.includes( preffered[ preffered.length - 1 ] )
-    )
+    let closestRight = interSection.filter( ( num ) => num <= preffered[ preffered.length - 1 ] );
+    if( ( interSection[ interSection.length - 1 ] > preffered[ preffered.length - 1 ] )
+      & !interSection.includes( preffered[ preffered.length - 1 ] ) )
       result.push( interSection[ interSection.length - 1 ] )
-    else result.push( Math.max( ... closestRight ) )
+    else result.push( Math.max( ... closestRight ) );
   }
-  //--------------------------------------------------------------
-  result = Array.from( new Set( result ) ) //state only uniqe elements of the result array
-  if( Math.max( ... allowed ) <= preffered[ 0 ] ) result = [ Math.max( ... result ) ]
 
-  return result
+  //------------------ //state only uniqe elements of the result array//--------------------------------------------
+
+  result = Array.from( new Set( result ) );
+  if( Math.max( ... allowed ) <= preffered[ 0 ] )
+    result = [ Math.max( ... result ) ];
+
+  return result;
 }
